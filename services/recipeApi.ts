8cadece60b76cee60922937338087911a0decf8d
Theresa Kreindl <s2410456015@fhooe.at>
2026-06-28 09:27:46 +0200

    28.06: Kommentare in Services

--- services/recipeApi.ts
+++ services/recipeApi.ts
@@ -1,22 +1,30 @@
 import { Recipe } from "../types/Recipe";
 import { translateIngredient } from "./ingredientTranslator";
 
+
+//Lädt passende Rezepte zu einer Zutat
 export async function fetchRecipesByIngredient(
     ingredient: string
 ): Promise<Recipe[]> {
+
+    //Übersetzung von deutsch in englisch
     const translatedIngredient =
         translateIngredient(ingredient);
 
+    //Anfrage (http get) an die API
     const response = await fetch(
         `https://www.themealdb.com/api/json/v1/1/filter.php?i=${translatedIngredient}`
     );
 
+    //Antwort wird umgewandelt von JSON in JavaScript- Objekt
     const data = await response.json();
 
+    //kein Rezept gefunden -> leeres Array
     if (!data.meals) {
         return [];
     }
 
+    //nur die ersten 5. Rezepte verwenden
     return data.meals.slice(0, 5).map((meal: any) => ({
         id: meal.idMeal,
         title: meal.strMeal,
@@ -30,34 +38,45 @@ export async function fetchRecipesByIngredient(
     }));
 }
 
+//Lädt die vollständigen Details eines Rezepts
 export async function fetchRecipeDetailsById(
     id: string
 ): Promise<Recipe | null> {
+    //Anfrage an API mit Rezept-ID -> alle Information zu dem Rezept geladen
     const response = await fetch(
         `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
     );
 
+    //wieder umwandeln
     const data = await response.json();
 
+    //keine Daten gefunden -> null
     if (!data.meals || data.meals.length === 0) {
         return null;
     }
 
+    //API liefert genau ein Rezept
     const meal = data.meals[0];
 
+    //später hier Zutaten speichern
     const ingredients: string[] = [];
 
+    //API besitzt max. 20 Zutatenfelder
     for (let i = 1; i <= 20; i++) {
+        //aktuelle Zutat auslesen
         const ingredient = meal[`strIngredient${i}`];
+        //wenn tatsächlich vorhanden wird diese übernommen
         const measure = meal[`strMeasure${i}`];
 
         if (ingredient && ingredient.trim().length > 0) {
+            //Menge + Zutat zusammenfügen
             ingredients.push(
                 `${measure ? measure.trim() : ""} ${ingredient.trim()}`
             );
         }
     }
 
+    //fertige Recipe-Objekt wird zurückgegeben
     return {
         id: meal.idMeal,
         title: meal.strMeal,
--- services/storage.ts
+++ services/storage.ts
@@ -1,20 +1,29 @@
+//lokaler Speicher auf Smartphone
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Recipe } from "../types/Recipe";
 
 const WISHLIST_KEY = "restlfest_wishlist";
 const FAVORITES_KEY = "restlfest_favorites";
 
+//Wunschliste laden
 export async function getWishlist(): Promise<Recipe[]> {
+   //gespeicherte daten auslesen
     const data = await AsyncStorage.getItem(WISHLIST_KEY);
+    //wenn Daten vorhanden-> in Array umgewandelt
     return data ? JSON.parse(data) : [];
 }
 
+//Rezept zur Wunschliste hinzufügen
 export async function addToWishlist(recipe: Recipe): Promise<void> {
+    //aktuelle Liste laden
     const wishlist = await getWishlist();
 
+    //prüfen, ob bereits vorhanden
     const alreadyExists = wishlist.some((item) => item.id === recipe.id);
 
+    //nur speichern, wenn es noch nciht existiert
     if (!alreadyExists) {
+        //am Ende anhängen und in JSON speichern
         await AsyncStorage.setItem(
             WISHLIST_KEY,
             JSON.stringify([...wishlist, recipe])
@@ -22,29 +31,41 @@ export async function addToWishlist(recipe: Recipe): Promise<void> {
     }
 }
 
+//Rezept aus der Wunschliste entfernen
 export async function removeFromWishlist(recipeId: string): Promise<void> {
+    //Wunschliste laden
     const wishlist = await getWishlist();
 
+    //alle Rezepte behalten, außer jenes mit der übergeben ID
     const updatedWishlist = wishlist.filter(
         (item) => item.id !== recipeId
     );
 
+    //neue Liste speichern
     await AsyncStorage.setItem(
         WISHLIST_KEY,
         JSON.stringify(updatedWishlist)
     );
 }
 
+//Favouriten laden
 export async function getFavorites(): Promise<Recipe[]> {
+    //aus AsyncStorage lesen
     const data = await AsyncStorage.getItem(FAVORITES_KEY);
+    //falls Daten vorhanden-> JSON wieder in Array umwandeln
     return data ? JSON.parse(data) : [];
 }
 
+
+//Rezept zu Favoriten hinzufügen
 export async function addToFavorites(recipe: Recipe): Promise<void> {
+    //Favoriten laden
     const favorites = await getFavorites();
 
+    //Prüfen, ob bereits exisiert
     const alreadyExists = favorites.some((item) => item.id === recipe.id);
 
+    //nur speichern, wenn noch nicht vorhanden
     if (!alreadyExists) {
         await AsyncStorage.setItem(
             FAVORITES_KEY,
@@ -53,29 +74,42 @@ export async function addToFavorites(recipe: Recipe): Promise<void> {
     }
 }
 
+//Rezept aus Favoriten entfernen
 export async function removeFromFavorites(recipeId: string): Promise<void> {
+    //Favoriten laden
     const favorites = await getFavorites();
 
+    //Gewünschtes Rezept entfernen
     const updatedFavorites = favorites.filter(
         (item) => item.id !== recipeId
     );
 
+    //Aktualisieren und speichern
     await AsyncStorage.setItem(
         FAVORITES_KEY,
         JSON.stringify(updatedFavorites)
     );
 }
 
+//Schlüssel für das zuletzt ausgwählte Rezept
 const SELECTED_RECIPE_KEY = "restlfest_selected_recipe";
 
+
+//Aktuell ausgewähltes Rezept speichern
 export async function saveSelectedRecipe(recipe: Recipe): Promise<void> {
+    //Rezept wird als JSON gespeichert
     await AsyncStorage.setItem(
         SELECTED_RECIPE_KEY,
         JSON.stringify(recipe)
     );
 }
 
+//zuletzt ausgewähltes Rezept laden
 export async function getSelectedRecipe(): Promise<Recipe | null> {
+    //Rezept aus AsyncStorage lesen
     const data = await AsyncStorage.getItem(SELECTED_RECIPE_KEY);
+
+    //Falls vorhanden-> JSON in Objekt umwandeln
+    //Falls nicht vorhanden -> null zurückgeben
     return data ? JSON.parse(data) : null;
 }
